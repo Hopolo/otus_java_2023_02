2785e7d4a3869caa8341773626841c2ebc760371
Yuriy Khodin <khodiniua@alfastrah.ru>
2023-03-22 15:28:47 +0300

    HW-02 using java collections

--- L04-generics/src/main/java/homework/Customer.java
+++ L04-generics/src/main/java/homework/Customer.java
@@ -1,6 +1,6 @@
 package homework;
 
-public class Customer {
+public class Customer implements Comparable<Customer>{
     private final long id;
     private String name;
     private long scores;
@@ -11,6 +11,12 @@ public class Customer {
         this.id = id;
         this.name = name;
         this.scores = scores;
+    }
+
+    public Customer(Customer customer) {
+        this.id = customer.id;
+        this.name = customer.name;
+        this.scores = customer.scores;
     }
 
     public long getId() {
@@ -48,17 +54,16 @@ public class Customer {
         if (o == null || getClass() != o.getClass()) return false;
 
         Customer customer = (Customer) o;
-
-        if (id != customer.id) return false;
-        if (scores != customer.scores) return false;
-        return name != null ? name.equals(customer.name) : customer.name == null;
+        return id == customer.id;
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (int) (scores ^ (scores >>> 32));
-        return result;
+        return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public int compareTo(Customer customer) {
+        return Long.compare(this.scores, customer.getScores());
     }
 }
