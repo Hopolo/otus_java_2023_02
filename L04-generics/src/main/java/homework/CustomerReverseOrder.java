b6cc421f6bae54e90b8766c25b3a027ed2d7b1e1
Yuriy Khodin <khodiniua@alfastrah.ru>
2023-03-22 14:51:35 +0300

    HW-02 using java collections

--- L04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ L04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,16 +1,19 @@
 package homework;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private Deque<Customer> order = new ArrayDeque<>();
 
     public void add(Customer customer) {
-
+        order.push(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return order.pop();
     }
 }
