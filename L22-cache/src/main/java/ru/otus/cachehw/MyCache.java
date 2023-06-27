644554beef1a9f0129fccc0dbc7a2c607ffdf9d0
Yuriy Khodin <khodiniua@alfastrah.ru>
2023-06-27 15:03:44 +0300

    HW-11 self-made cache

--- L22-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ L22-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,31 +1,41 @@
 package ru.otus.cachehw;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public class MyCache<K, V> implements HwCache<K, V> {
 //Надо реализовать эти методы
 
-    @Override
-    public void put(K key, V value) {
+    private final List<HwListener<K, V>> listeners = new ArrayList<>();
+    private final Map<K, V> cacheMap = new WeakHashMap<>();
 
+    @Override
+    public void put(
+        K key,
+        V value
+    ) {
+        cacheMap.put(key, value);
     }
 
     @Override
     public void remove(K key) {
-
+        cacheMap.remove(key);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return cacheMap.get(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-
+        listeners.remove(listener);
     }
 }
